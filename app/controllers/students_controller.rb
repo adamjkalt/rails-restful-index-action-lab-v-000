4e56440ba0b501e9328b562cfd9636561481d083
Adam Kalt <adam.kalt.1@gmail.com>
2018-08-20 20:22:19 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,10 @@
 class StudentsController < ApplicationController
+
   def index
     @students = Student.all
   end
+
+  get '/students'
+    @students = Student.all
+    erb :'students/index'
 end
